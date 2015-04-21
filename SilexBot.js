e4a1f90356e351e5bf570e3024fd6cb546b593f7
Jean-Baptiste Richardet <jbrichardet@tf1.fr>
2015-04-21 11:45:41 +0200

    Construct a farm
    Ajout du Dojo du 20/04/2015

--- SilexBot.js
+++ SilexBot.js
@@ -12,49 +12,78 @@ m.SilexBot = function(settings) {
 	this.savedEvents = [];
 	
 	this.toUpdate = [];
-}
+};
 
 m.SilexBot.prototype = new API3.BaseAI();
 
-//m.SilexBot.prototype.state = 0;
+m.SilexBot.prototype.buildFarm = function( f_workers ) {
+
+	var nbFarm = 0;
+	var gameState = this.gameState;
+	
+	f_workers.forEach(function(builder){
+		if(builder.isIdle()){
+			var buildings = gameState.getOwnFoundations().toEntityArray(); 
+			if (buildings.length > 0){
+				builder.repair(buildings[0]);
+			} else if ( nbFarm < 1 ) {
+				var x = builder.position()[0] + Math.round( Math.random() * 200 - 100);
+				var y = builder.position()[1] + Math.round( Math.random() * 200 - 100 );
+				var building = builder.construct("structures/athen_field", x, y, 0);
+				nbFarm++;
+			}
+		}
+	});
+
+};
+
 m.SilexBot.prototype.OnUpdate = function() {
-	/*if(this.gameFinished){
+	if(this.gameFinished){
 		return;
-	}*/
+	}
 
 	API3.DebugEnabled = true;
 	
+	// Workers
+	var f_workers = this.gameState.updatingCollection("f_workers", 
+		API3.Filters.and(
+			API3.Filters.byClass("Worker"),
+			API3.Filters.byClass("Female")),
+		this.gameState.getOwnUnits());
+	var m_workers = this.gameState.updatingCollection("m_workers", 
+		API3.Filters.and(
+			API3.Filters.byClass("Worker"),
+			API3.Filters.not(
+				API3.Filters.byClass("Female"))),
+		this.gameState.getOwnUnits());
+
+
+	// BUILD A FARM
+	var gameState = this.gameState;
 	
-	
-		var woods = this.gameState.getResourceSupplies("wood");
-		var ownunits = this.gameState.getOwnUnits();
-		var woodChoppers = ownunits.filter(API3.Filters.not(API3.Filters.isSoldier()));
-		API3.debug("Found workers : "+woodChoppers.length);
-		API3.debug("Found woods : " + woods.length);
-		//woodChoppers[0].gather(woods[0]);
-		
-		woodChoppers.forEach(function(woodChopper) {
+	var field_count = gameState.countEntitiesByType("structures/athen_field");
 
-			//wood
-			var closestWood = woods.filterNearest(woodChopper.position(),1).toEntityArray();
+	if ( field_count < 1 )
+	{
+		this.buildFarm( f_workers );
+	}
 
-			if((woodChopper.isIdle()) && (closestWood.length >0)){
-				woodChopper.gather(closestWood[0]);
-			}
-		});
-		
-		//woodChoppers[0].setMetaData("")
+	// FOOD
+	var foods = this.gameState.getResourceSupplies("food");
 
+	m_workers.forEach(function(foodGatherer) {
+		var closestFood = foods.filterNearest(foodGatherer.position(),1).toEntityArray();
 
-
-
+		if((foodGatherer.isIdle()) && (closestFood.length >0)){
+			foodGatherer.gather(closestFood[0]);
+		}
+	});
 };
 
 m.debug = function(output)
 {
 	API3.debug(output);
-}
-
+};
 
 return m;
 }());
